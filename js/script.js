e0cd0ab10c75bcb5a84311e003eb1221086057fd
Abdullah <graphicdesigner.abdullah@gmail.com>
2024-08-28 15:37:14 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Tailwindcss cdn
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        darkColor: "#14191e",
+      },
+      fontFamily: {
+        manrope: ["Manrope", "sans-serif"],
+      },
+    },
+  },
+};
+// "use strict";
